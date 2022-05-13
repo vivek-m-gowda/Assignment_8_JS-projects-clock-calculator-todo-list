e9dcffc2c4979ef29f99898fbe621250f45b2129
vivek-m-gowda <vivekgowda880@gmail.com>
2022-05-13 13:16:43 +0530

    push code

---
+++ Digital clock/clock.js
@@ -0,0 +1,29 @@
+
+function showTime()
+{
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = "AM";
+    // console.log(date);
+    
+    if(h==0)
+    {
+        h=12;
+    }
+            
+    if(h>12)
+    {
+        h= h-12;
+        session = "PM";
+    }
+            
+    h = (h<10)?"0"+h:h;
+    m = (m<10)?"0"+m:m;
+    s = (s<10)?"0"+s:s;
+    document.getElementById("DigitalClock").innerHTML = h + ":" + m + ":" + s + session;
+    setTimeout(showTime,1000);
+}
+
+showTime();
---
+++ To Do List/To_Do_List.js
@@ -0,0 +1,21 @@
+function add_item()
+{
+    let item = document.getElementById("box");
+    let list_item = document.getElementById("list_item");
+    if(item.value != "")
+    {
+        let make_li = document.createElement("li");
+        make_li.appendChild(document.createTextNode(item.value));
+        list_item.appendChild(make_li);
+        item.value="";
+
+        make_li.onclick = function()
+        {
+            this.parentNode.removeChild(this);
+        }
+    }
+    else
+    {
+        alert("please add a value to item");
+    }
+}
